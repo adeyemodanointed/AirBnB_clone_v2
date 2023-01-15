60992c18a59f0d0c65316c2be191ff0f0561de7b
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-01-15 21:30:28 +0000

    task 3, web framework

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Start a flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Return string when route '/' is queried"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route /hbnb is queried"""
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>')
+def cisfun(text):
+    """Return C with the sring passed"""
+    return 'C %s' % text.replace('_', ' ')
+
+@app.route('/python/')
+@app.route('/python/<string:text>')
+def python_route(text='is cool'):
+    """Return Python with the string passed"""
+    return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
+    app.url_map.strict_slashes = False
