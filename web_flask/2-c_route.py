a74d9c90ec7e211d85674184b5651024f2bbaa23
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-01-15 21:08:58 +0000

    Task 2, web framework

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,17 +4,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Return string when route '/' is queried"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
+    """Return string when route /hbnb is queried"""
     return 'HBNB'
 
-@app.route('/c/<text>'):
-    def cisfun():
-        
+
+@app.route('/c/<string:text>')
+def cisfun(text):
+    """Return C with the sring passed"""
+    return 'C %s' % text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
