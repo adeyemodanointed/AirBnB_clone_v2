618ff10525b3bb79ef4218993dab8b3a41d2f438
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-01-15 20:54:02 +0000

    task 1, web framework

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Start a flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>'):
+    def cisfun():
+        
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
+    app.url_map.strict_slashes = False
